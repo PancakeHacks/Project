ea7214cea6711cf911f528e77ee25be7ed57fcde
csDannyy <1217dannyy@gmail.com>
2024-06-21 22:28:12 -0500

    testing

--- Pygame/test.py
+++ Pygame/test.py
@@ -12,6 +12,7 @@ myFont = pygame.font.SysFont("Segoe UI", 90)
 helloWorld = myFont.render("Hello World", 1, (255, 0, 255), (255, 255, 255))
 
 while 1:
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT: sys.exit()
 windowsSize.blit(helloWorld, (0, 0))
